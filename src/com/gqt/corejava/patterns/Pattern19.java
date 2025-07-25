fb231b6c69a2a7796bd9bdb317aeb6524659b396
USER <USER@spsh103>
2025-07-25 20:31:32 +0530

    second commit

--- src/com/gqt/corejava/patterns/Pattern19.java
+++ src/com/gqt/corejava/patterns/Pattern19.java
@@ -1,10 +1,24 @@
 package com.gqt.corejava.patterns;
-
+import java.util.Scanner;
 public class Pattern19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		        Scanner scanner = new Scanner(System.in);
 
-	}
+		        System.out.print("Enter the number of elements in the first row: ");
+		        int rows = scanner.nextInt();
 
-}
+		        int number = 1;
+
+		        for (int i = rows; i >= 1; i--) {
+		            for (int j = 1; j <= i; j++) {
+		                System.out.print(number++ + "	 ");
+		            }
+		            System.out.println();
+		        }
+
+		        scanner.close();
+		    }
+		}
+
